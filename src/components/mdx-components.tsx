1f7c020ee47d22982772fa3b6671b46c2452783e
Iwan Efendi <iwan.efndi@gmail.com>
2026-02-16 05:45:00 +0000

    Try fixing this error: `Console Error: In HTML, <div> cannot be a descen

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -49,7 +49,7 @@ const MdxA = (props: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   return <a {...props} />;
 };
 const MdxUl = ({ children }: { children?: React.ReactNode }) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>;
-const MdxOl = ({ children }: { children?: React.ReactNode }) => <ol className="my-6 ml-6 list-decimal [&>li]:mt-2">{children}</ol>;
+const MdxOl = ({ children }: { children?: React.ReactNode }) => <ol className="my-6 ml-6 list-decimal [&>li]:mt-2">{children}</ul>;
 const MdxLi = ({ children }: { children?: React.ReactNode }) => <li>{children}</li>;
 const MdxBlockquote = ({ children }: { children?: React.ReactNode }) => <blockquote className="mt-6 border-l-2 border-primary/20 pl-6 italic text-muted-foreground">{children}</blockquote>;
 
