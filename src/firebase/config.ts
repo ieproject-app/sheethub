370e3379b881f71c1afa36a130bc7072c7b2f7d6
Iwan Efendi <iwan.efndi@gmail.com>
2026-03-01 07:41:53 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAtcWDqJ28CI3Pc4pHDN5ET7iTsgmxe2-E",
-  authDomain: "studio-8697076532-14512.firebaseapp.com",
-  projectId: "studio-8697076532-14512",
-  storageBucket: "studio-8697076532-14512.firebasestorage.app",
-  messagingSenderId: "910122792787",
-  appId: "1:910122792787:web:183c7da1ae114ac74ad3a6"
+  "projectId": "studio-8697076532-14512",
+  "appId": "1:910122792787:web:183c7da1ae114ac74ad3a6",
+  "apiKey": "AIzaSyAtcWDqJ28CI3Pc4pHDN5ET7iTsgmxe2-E",
+  "authDomain": "studio-8697076532-14512.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "910122792787"
 };
